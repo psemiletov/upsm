40fb7dbb1facfd3133dd37554014561b0cc17b64
Petr Semiletov <tea@list.ru>
2017-07-14 14:30:58 +0300

    translation update

--- translations/upsm_ru.ts
+++ translations/upsm_ru.ts
@@ -4,32 +4,57 @@
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../mainwindow.cpp" line="117"/>
+        <source>Stats</source>
+        <translation>Статистика</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="134"/>
+        <source>Command</source>
+        <translation>Команда</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>Polling time, msecs</source>
+        <translation>Время опроса, в миллисекундах</translation>
+    </message>
+    <message>
         <location filename="../mainwindow.cpp" line="150"/>
+        <source>Apply and save</source>
+        <translation>Применить и сохранить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="213"/>
         <source>Input: </source>
         <translation>Вход:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="155"/>
+        <location filename="../mainwindow.cpp" line="218"/>
         <source>Output: </source>
         <translation>Выход:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="159"/>
+        <location filename="../mainwindow.cpp" line="222"/>
         <source>Load: </source>
         <translation>Нагрузка:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="174"/>
+        <location filename="../mainwindow.cpp" line="237"/>
         <source>voltage normal</source>
         <translation>напряжение нормальное</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="185"/>
+        <location filename="../mainwindow.cpp" line="248"/>
         <source>ups is trimming voltage</source>
         <translation>ИБП понижает напряжение</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="195"/>
+        <location filename="../mainwindow.cpp" line="258"/>
         <source>battery mode</source>
         <translation>работа от батареи</translation>
     </message>
